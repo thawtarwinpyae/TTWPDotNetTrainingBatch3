d38a58584535d5b7fa7b3a45bfb570f38b0305a7
thawt <thawt@DESKTOP-SO3OTUO>
2025-10-11 11:09:12 +0630

    feat: ado.net read

---
+++ TTWPDotNetTrainingBatch3.ConsoleApp2/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+Console.WriteLine("Hello, World!");
+
+SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+
+sqlConnectionStringBuilder.DataSource="."; // server name
+sqlConnectionStringBuilder.InitialCatalog = "MiniPOS";//database name
+sqlConnectionStringBuilder.UserID = "sa";
+sqlConnectionStringBuilder.Password = "sasa@123";
+sqlConnectionStringBuilder.TrustServerCertificate = true;
+
+SqlConnection connection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString);
+
+connection.Open();
+
+Console.WriteLine("hello");
+
+string query = @"SELECT [ProductId]
+      ,[ProductName]
+      ,[Quantity]
+      ,[Price]
+      ,[DeleteFlag]
+  FROM [dbo].[Tbl_Product]";
+
+SqlCommand cmd = new SqlCommand(query, connection);
+SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+DataTable dt = new DataTable();
+adapter.Fill(dt);
+
+connection.Close();
+
+Console.WriteLine("---Product List---");
+
+for (int i = 0; i<dt.Rows.Count; i++)
+{
+    DataRow row = dt.Rows[i];
+
+    int rowNo = i+1;
+    decimal price =Convert.ToDecimal(row["Price"]);
+
+    /*Console.WriteLine(row["ProductId"]);
+    Console.WriteLine(row["ProductName"]);
+    Console.WriteLine(row["Quantity"]);
+    Console.WriteLine(row["Price"]);*/
+    Console.WriteLine(rowNo.ToString() + ". " +row["ProductName"].ToString()+"("+price.ToString("n0") +")");
+
+}
+
+Console.ReadLine();
